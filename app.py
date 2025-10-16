42cdcfb6da6fd13c082234938eb67517afe09913
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-10-17 04:57:55 +0530

    extracted df from chat

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 import streamlit as st
- 
+import preprocessor
+
+st.sidebar.title("Whatsapp chat analyzer")
+
+
+uploaded_file = st.sidebar.file_uploader("Choose a file")
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    data = bytes_data.decode("utf-8")
+    st.text(data)
+    df = preprocessor.preprocessor(data)
+
+    st.dataframe(df)
--- preprocessor.py
+++ preprocessor.py
@@ -1,2 +1,43 @@
+import re
+
+
+import pandas as pd
+
+
 def preprocessor(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    pattern = r'\d{1,2}/\d{1,2}/\d{2},\s\d{1,2}:\d{2}\s-\s'
+
+    messages= re.split(pattern,data)[1:]
+    dates=re.findall(pattern,data)
+
+    df=pd.DataFrame({'user_message':messages,'message_date':dates})
+    df['message_date'] = pd.to_datetime(df['message_date'].str.strip(), format='%m/%d/%y, %H:%M -', errors='coerce')
+
+    df.rename(columns={'message_date':'date'},inplace=True)
+
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split(r'^(.*?):\s', message, maxsplit=1)
+        if len(entry) > 2:
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df['only_date'] = df['date'].dt.date
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+    df.drop(columns=['user_message'], inplace=True)
+
+
+
+    return df
