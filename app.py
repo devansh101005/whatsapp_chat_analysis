f871fa4ce725d7318455697ce5a1500a88188073
Devansh Pandey <devanshpandeyji4321@gmail.com>
2025-11-07 10:25:25 +0530

    added busy users code

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import preprocessor,helper
+import matplotlib.pyplot as plt
 
 st.sidebar.title("Whatsapp chat analyzer")
 
@@ -21,11 +22,11 @@ if uploaded_file is not None:
     user_list.sort()
     user_list.insert(0,"Overall")
 
-    selected_user=st.sidebar.selectbox("Show ananlysis wrt",user_list)
+    selected_user=st.sidebar.selectbox("Show analysis wrt",user_list)
 
     if st.sidebar.button("Show Analysis"):
 
-        num_messages,words,num_media_messages=helper.fetch_stats(selected_user,df)
+        num_messages,words,num_media_messages,num_links=helper.fetch_stats(selected_user,df)
 
         col1,col2,col3,col4=st.columns(4)
 
@@ -40,5 +41,25 @@ if uploaded_file is not None:
         with col3:
             st.header("Media messages")
             st.title(num_media_messages)
+
+        with col4:
+            st.header("Links Shared")
+            st.title(num_links)
+
+
+            #finding the busiest user in group(Group level )
+
+        if selected_user=="Overall":
+            st.title('Most Busy User')
+            x,new_df=helper.most_busy_users(df)
+            fig, ax = plt.subplots()
+            col1,col2 =st.columns(2)
+
+            with col1:
+                ax.bar(x.index, x.values, color='red')
+                plt.xticks(rotation='vertical')
+                st.pyplot(fig)
+            with col2:
+                st.dataframe(new_df)
 
 
--- helper.py
+++ helper.py
@@ -1,3 +1,7 @@
+from urlextract import URLExtract
+extract=URLExtract()
+
+
 def fetch_stats(selected_user,df):
 
     # if(selected_user=='Overall'):
@@ -32,5 +36,18 @@ def fetch_stats(selected_user,df):
 
         # fetch number of media messages
         num_media_messages = df[df['message'] == '<Media omitted>\n'].shape[0]
+        
+        #fetch number of links shared
+        links=[]
+        for message in df['message']:
+            links.extend(extract.find_urls(message))
 
-        return num_messages,len(words),num_media_messages
+
+        return num_messages,len(words),num_media_messages,len(links)
+
+
+def most_busy_users(df):
+    x = df['user'].value_counts().head()
+    df = round((df['user'].value_counts() / df.shape[0]) * 100, 2).reset_index().rename(
+        columns={'index': 'name', 'user': 'percent'})
+    return x,df
